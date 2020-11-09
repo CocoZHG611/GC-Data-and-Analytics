f40a3cf9658912c0c44e29e197ff588d7b498284
Coco Zhang <48548697+CocoZHG611@users.noreply.github.com>
2020-11-09 11:46:25 +0800

    Add files via upload

--- Project_Details_RPA_Channel.sql
+++ Project_Details_RPA_Channel.sql
@@ -59,22 +59,24 @@ WITH d_emp AS (select
                 and u_latest.END_DATE_KEY = 99991231  )
 
 
+
 SELECT
 	d_date_proj.DATE AS CREATE_DATE,
 	f_project.PROJECT_ID  AS proj_id,
 	f_project.TITLE  AS proj_title,
 	d_emp_pcsp.EMPLOYEENAME  AS employee_name,
+	d_emp_pcsp.PERSON_ID AS employee_ID,
 	d_emp_pcsp.POD  AS pod,
 	d_emp_pcsp.BU  AS bu,
 	d_tpv_client.CLIENT_ID  AS client_id,
 	d_tpv_client.CLIENT_NAME  AS client_name,
 	cedp.PUBLISH_CHANNEL AS Publish_Channel,
-	TPV.FIRST_TPV_DATE,
 	SA.STATUS_DATE AS ATTACHED_DATE,
 	SG.STATUS_DATE AS GTC_DATE,
-	ISNULL(DATEDIFF(DAY, d_date_proj.DATE, TPV.FIRST_TPV_DATE),0) as tpv_speed,
+	TPV.FIRST_TPV_DATE,
 	ISNULL(DATEDIFF(DAY, d_date_proj.DATE, SA.STATUS_DATE),0) as ATTACH_speed,
 	ISNULL(DATEDIFF(DAY, d_date_proj.DATE, SG.STATUS_DATE),0) as GTC_speed,
+	ISNULL(DATEDIFF(DAY, d_date_proj.DATE, TPV.FIRST_TPV_DATE),0) as tpv_speed,
 	COUNT(DISTINCT f_tpv.F_TPV_KEY ) AS tpv_count,
 	COUNT(DISTINCT CASE WHEN (f_tpv.RP_STATUS = 'CRP') THEN f_tpv.F_TPV_KEY   ELSE NULL END) AS tpv_crp_1
 into #Temp
@@ -111,6 +113,7 @@ d_date_proj.DATE,
 d_tpv_client.CLIENT_SFDC_ID ,
 f_project.PROJECT_ID ,
 f_project.TITLE ,
+d_emp_pcsp.PERSON_ID ,
 d_emp_pcsp.EMPLOYEENAME ,
 d_emp_pcsp.POD ,d_emp_pcsp.BU ,
 d_tpv_client.CLIENT_ID ,
@@ -128,27 +131,27 @@ order by f_project.PROJECT_ID*/
 
 select * from #Temp 
 where Publish_Channel='recruiting partner'
-order by proj_id, CREATE_DATE
+order by employee_ID,proj_id, CREATE_DATE
 
 select a.*
 	, cast(cast(a.success_proj as decimal(18,2))/a.total_proj as decimal(18,2)) as success_rate 
 	, cast(cast(a.total_tpv as decimal(18,2))/a.total_proj as decimal(18,2)) as yield 
-	, cast(cast(a.total_tpv_speed as decimal(18,2))/a.tpv_speed_proj as decimal(18,2)) as TPV_speed 
 	, cast(cast(a.total_ATTACH_speed as decimal(18,2))/a.ATTACH_speed_proj as decimal(18,2)) as ATTACH_speed
 	, cast(cast(a.total_GTC_speed as decimal(18,2))/a.GTC_speed_proj as decimal(18,2)) as GTC_speed
+	, cast(cast(a.total_tpv_speed as decimal(18,2))/a.tpv_speed_proj as decimal(18,2)) as TPV_speed 
 from( 
 select Year(CREATE_DATE) as CREATE_YEAR
 	, MONTH(CREATE_DATE) as CREATE_MONTH
 	, Publish_Channel
 	, count(proj_id) as total_proj
 	, COUNT(CASE WHEN tpv_count<>0 THEN tpv_count END) as success_proj
-	, COUNT(CASE WHEN tpv_speed<>0 THEN tpv_speed END) as tpv_speed_proj
 	, COUNT(CASE WHEN ATTACH_speed<>0 THEN ATTACH_speed END) as ATTACH_speed_proj
 	, COUNT(CASE WHEN GTC_speed<>0 THEN GTC_speed END) as GTC_speed_proj
+	, COUNT(CASE WHEN tpv_speed<>0 THEN tpv_speed END) as tpv_speed_proj
 	, sum(tpv_count) as total_tpv
-	, sum(tpv_speed) as total_tpv_speed
 	, sum(ATTACH_speed) as total_ATTACH_speed
 	, sum(GTC_speed) as total_GTC_speed
+	, sum(tpv_speed) as total_tpv_speed
 from #Temp
 group by Year(CREATE_DATE)
 	, MONTH(CREATE_DATE)
@@ -159,3 +162,72 @@ order by a.Publish_Channel, a.CREATE_Year
 
 --select distinct PROJECT_ACTIVITY_STATUS from WARS.bi.D_PROJECT_ACTIVITY_STATUS
 --order by PROJECT_ACTIVITY_STATUS
+
+select a.*
+	, cast(cast(a.success_proj as decimal(18,2))/a.total_proj as decimal(18,2)) as success_rate 
+	, cast(cast(a.total_tpv as decimal(18,2))/a.total_proj as decimal(18,2)) as yield 
+	, cast(cast(a.total_ATTACH_speed as decimal(18,2))/a.ATTACH_speed_proj as decimal(18,2)) as ATTACH_speed
+	, cast(cast(a.total_GTC_speed as decimal(18,2))/a.GTC_speed_proj as decimal(18,2)) as GTC_speed
+	, cast(cast(a.total_tpv_speed as decimal(18,2))/a.tpv_speed_proj as decimal(18,2)) as TPV_speed 
+from( 
+select Year(CREATE_DATE) as CREATE_YEAR
+	, MONTH(CREATE_DATE) as CREATE_MONTH
+	, pod
+	, Publish_Channel
+	, count(proj_id) as total_proj
+	, COUNT(CASE WHEN tpv_count<>0 THEN tpv_count END) as success_proj
+	, COUNT(CASE WHEN ATTACH_speed<>0 THEN ATTACH_speed END) as ATTACH_speed_proj
+	, COUNT(CASE WHEN GTC_speed<>0 THEN GTC_speed END) as GTC_speed_proj
+	, COUNT(CASE WHEN tpv_speed<>0 THEN tpv_speed END) as tpv_speed_proj
+	, sum(tpv_count) as total_tpv
+	, sum(ATTACH_speed) as total_ATTACH_speed
+	, sum(GTC_speed) as total_GTC_speed
+	, sum(tpv_speed) as total_tpv_speed
+from #Temp
+where pod='FS - Beijing FS' or pod='FS - Shanghai FS' or pod='FS - South China FS' or pod='FS - Greater China Private Equity'
+group by Year(CREATE_DATE)
+	, MONTH(CREATE_DATE)
+	, pod
+	, Publish_Channel) a
+order by a.Publish_Channel, a.pod, a.CREATE_Year
+	, a.CREATE_MONTH
+
+select a.*
+	, ISNULL(cast(cast(a.success_proj as decimal(18,2))/NULLIF(a.total_proj,0) as decimal(18,2)),0) as success_rate 
+	, ISNULL(cast(cast(a.total_tpv as decimal(18,2))/NULLIF(a.total_proj,0) as decimal(18,2)),0) as yield 
+	, ISNULL(cast(cast(a.total_ATTACH_speed as decimal(18,2))/NULLIF(a.ATTACH_speed_proj,0) as decimal(18,2)),0) as ATTACH_speed
+	, ISNULL(cast(cast(a.total_GTC_speed as decimal(18,2))/NULLIF(a.GTC_speed_proj,0) as decimal(18,2)),0) as GTC_speed
+	, ISNULL(cast(cast(a.total_tpv_speed as decimal(18,2))/NULLIF(a.tpv_speed_proj,0) as decimal(18,2)),0) as TPV_speed 
+from( 
+select distinct employee_ID
+	, Publish_Channel
+	, count(proj_id) as total_proj
+	, COUNT(CASE WHEN tpv_count<>0 THEN tpv_count END) as success_proj
+	, COUNT(CASE WHEN ATTACH_speed<>0 THEN ATTACH_speed END) as ATTACH_speed_proj
+	, COUNT(CASE WHEN GTC_speed<>0 THEN GTC_speed END) as GTC_speed_proj
+	, COUNT(CASE WHEN tpv_speed<>0 THEN tpv_speed END) as tpv_speed_proj
+	, sum(tpv_count) as total_tpv
+	, sum(ATTACH_speed) as total_ATTACH_speed
+	, sum(GTC_speed) as total_GTC_speed
+	, sum(tpv_speed) as total_tpv_speed
+from #Temp
+group by employee_ID
+	, Publish_Channel) a
+order by a.Publish_Channel, a.employee_ID
+
+select a.*
+	, cast(cast(a.success_proj as decimal(18,2))/a.total_proj as decimal(18,2)) as success_rate 
+	, cast(cast(a.total_tpv as decimal(18,2))/a.total_proj as decimal(18,2)) as yield 
+	, cast(cast(a.total_proj-LAG(a.total_proj) OVER (ORDER BY a.CREATE_YEAR, a.CREATE_MONTH) as decimal(18,2))/ LAG(a.total_proj) OVER (ORDER BY a.CREATE_YEAR, a.CREATE_MONTH ) as decimal(18,2)) AS total_growth 
+from( 
+select Year(CREATE_DATE) as CREATE_YEAR
+	, MONTH(CREATE_DATE) as CREATE_MONTH
+	, count(proj_id) as total_proj
+	, COUNT(CASE WHEN tpv_count<>0 THEN tpv_count END) as success_proj
+	, sum(tpv_count) as total_tpv
+from #Temp
+where Publish_Channel='recruiting partner'
+group by Year(CREATE_DATE)
+	, MONTH(CREATE_DATE)) a
+order by a.CREATE_Year
+	, a.CREATE_MONTH
